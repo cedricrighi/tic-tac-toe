539926f30d17397b5f23e9e8847cc3b75949b541
cedricrighi <cedric.righi@gmail.com>
2025-05-15 20:13:56 +0200

    feat: create home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TitlebarComponent } from './titlebar/titlebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TitlebarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'tic-tac-toe';
---
+++ src/app/titlebar/titlebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-titlebar',
+  imports: [],
+  templateUrl: './titlebar.component.html',
+  styleUrl: './titlebar.component.css'
+})
+export class TitlebarComponent {
+
+}
