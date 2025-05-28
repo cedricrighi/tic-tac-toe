dbf86a714d7b7a58def675c977a546937a796fa2
cedricrighi <cedric.righi@gmail.com>
2025-05-28 10:26:04 +0200

    fix: start the logic of vsbot component

--- src/app/gamevsbot/gamevsbot.component.ts
+++ src/app/gamevsbot/gamevsbot.component.ts
@@ -4,8 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-gamevsbot',
   imports: [],
   templateUrl: './gamevsbot.component.html',
-  styleUrl: './gamevsbot.component.css'
+  styleUrl: './gamevsbot.component.css',
 })
 export class GamevsbotComponent {
+  board: string[] = Array(9).fill(' ');
+  actualPlayer = 'X';
 
+  play(index: number) {
+    if (this.board[index] !== ' ') {
+      return;
+    }
+    console.log(this.board);
+    console.log(`case: ${index}`);
+    this.board[index] = this.actualPlayer;
+    this.actualPlayer = this.actualPlayer === 'X' ? 'O' : 'X';
+  }
 }
